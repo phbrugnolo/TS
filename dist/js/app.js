d73880d68f2bb30b0e0a8e1f87a4690ea816b7a2
jlcomar <joaolucascomar@gmail.com>
2024-04-05 17:29:20 -0300

    iniciando e finalizando o projeto

---
+++ dist/js/app.js
@@ -0,0 +1,22 @@
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+if (form) {
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        controller.adiciona();
+    });
+}
+else {
+    throw Error("Não foi possivel inicializar a aplicação. Verifique se o form existe");
+}
+;
+const botaoImport = document.querySelector('#botao-import');
+if (botaoImport) {
+    botaoImport.addEventListener('click', () => {
+        controller.importaDados();
+    });
+}
+else {
+    throw Error('Botão import não foi encontrado');
+}
